1f7e51c9b32e066b01dde99ff4adad56099824b7
cham207388 <cham207388@gmail.com>
2026-08-14 18:42:38 -0500

    token advisors

--- src/main/java/com/abcham/springai/controller/ChatController.java
+++ src/main/java/com/abcham/springai/controller/ChatController.java
@@ -1,9 +1,11 @@
 package com.abcham.springai.controller;
 
+import com.abcham.springai.advisor.TokenUsageAuditAdvisor;
 import com.abcham.springai.utils.Constants;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,9 +21,9 @@ public class ChatController {
 
     @GetMapping("/chat")
     public String chat(@RequestParam String message) {
-        log.info("message: {}", message);
         return chatClient
                 .prompt()
+                .advisors(new TokenUsageAuditAdvisor())
                 .user(message)
                 .call()
                 .content();
