4617cc45d3f9a2058e14308b5bc688de65e23403
cham207388 <cham207388@gmail.com>
2026-08-14 12:43:17 -0500

    working on it

---
+++ src/main/java/com/abcham/springai/config/ChatClientConfig.java
@@ -0,0 +1,18 @@
+package com.abcham.springai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+
+    @Bean
+    public ChatClient openAiChatClient(OpenAiChatModel openAiChatModel) {
+        return ChatClient.create(openAiChatModel);
+    }
+
+    @Bean
+    public ChatClient bedRockChatClient() {}
+}
