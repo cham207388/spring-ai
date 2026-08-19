68eb8e4a91b2957602c40188a0f0e43c7ba8d39a
cham207388 <cham207388@gmail.com>
2026-08-19 14:07:33 -0500

    chat memory persist using postgrs

--- src/main/java/com/abcham/springai/controller/ChatMemoryController.java
+++ src/main/java/com/abcham/springai/controller/ChatMemoryController.java
@@ -4,13 +4,11 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@RequestMapping("/api")
 public class ChatMemoryController {
 
     private final ChatClient chatClient;
