2ecf04b0681820c489854adb61c72cacf1598dfc
cham207388 <cham207388@gmail.com>
2026-08-20 15:59:57 -0500

    this version has no transformer

--- src/main/java/com/abcham/springai/config/ChatClientConfig.java
+++ src/main/java/com/abcham/springai/config/ChatClientConfig.java
@@ -12,7 +12,6 @@ import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
-import org.springframework.ai.rag.preretrieval.query.transformation.TranslationQueryTransformer;
 import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
@@ -62,13 +61,9 @@ public class ChatClientConfig {
     }
 
     @Bean
-    RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(VectorStore vectorStore,
-                                                              ChatClient.Builder chatClientBuilder) {
+    RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(VectorStore vectorStore) {
 
         return RetrievalAugmentationAdvisor.builder()
-                .queryTransformers(TranslationQueryTransformer.builder()
-                        .chatClientBuilder(chatClientBuilder.clone())
-                        .targetLanguage("english").build())
                 .documentRetriever(VectorStoreDocumentRetriever.builder().vectorStore(vectorStore)
                         .topK(3).similarityThreshold(0.5).build())
                 .documentPostProcessors(PIIMaskingDocumentPostProcessor.builder())
@@ -77,7 +72,7 @@ public class ChatClientConfig {
 
     @Bean("webSearchRAGChatClient")
     public ChatClient webSearchRAGChatClient(ChatClient.Builder chatClientBuilder,
-                                 ChatMemory chatMemory, RestClient.Builder restClientBuilder) {
+                                             ChatMemory chatMemory, RestClient.Builder restClientBuilder) {
 
         Advisor loggerAdvisor = new SimpleLoggerAdvisor();
         Advisor tokenUsageAdvisor = new TokenUsageAuditAdvisor();
